684ff6cbbbb2bca3bb5b70c46622a27d19270d9f
HWulfstan <totalwar.civ@gmail.com>
2025-12-16 01:57:09 +0100

    Add Ansible lab

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.12-slim
-WORKDIR /app
-COPY . .
-RUN pip install django
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
--- twitter_clone/twitter_app/settings.py
+++ twitter_clone/twitter_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,13 +74,15 @@ WSGI_APPLICATION = 'twitter_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
@@ -135,3 +138,8 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import os
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
